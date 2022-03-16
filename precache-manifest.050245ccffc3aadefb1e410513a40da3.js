ac353427d52956566ede08e8a09db11ea15f2272
jake langhel <jacobtlanghel@gmail.com>
2022-03-16 13:04:27 -0400

    Updates

---
+++ precache-manifest.050245ccffc3aadefb1e410513a40da3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "456cf13c943cb82d4890",
+    "url": "/huddle-landing-page/static/css/main.9d56acf8.chunk.css"
+  },
+  {
+    "revision": "456cf13c943cb82d4890",
+    "url": "/huddle-landing-page/static/js/main.456cf13c.chunk.js"
+  },
+  {
+    "revision": "1a8102be3db988de677c",
+    "url": "/huddle-landing-page/static/js/1.1a8102be.chunk.js"
+  },
+  {
+    "revision": "dbd5e85abc069c91480b",
+    "url": "/huddle-landing-page/static/js/runtime~main.dbd5e85a.js"
+  },
+  {
+    "revision": "fcc189d282244f18046574b7cf82ae2f",
+    "url": "/huddle-landing-page/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/huddle-landing-page/precache-manifest.050245ccffc3aadefb1e410513a40da3.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/huddle-landing-page/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
